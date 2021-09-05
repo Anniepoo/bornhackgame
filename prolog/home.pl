2c9f45378ab72e0e0cc7b7e8a62b69f8e8d63f1b
Anne Ogborn <annie66us@yahoo.com>
2021-09-05 11:48:55 +0200

    home.pl no longer needed

--- prolog/home.pl
+++
@@ -1,8 +0,0 @@
-:- module(home, [
-              content//0
-          ]).
-
-:- use_module(library(http/html_write)).
-:- use_module(library(http/http_session)).
-:- use_module(library(http/js_write)).
-:- use_module(library(http/http_json)).
